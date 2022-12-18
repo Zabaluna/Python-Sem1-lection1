f65fe2dd4f6df45dad45caf81070d6c73f1b8500
Наталья Ярцева <zabaluna@mail.ru>
2022-12-18 18:30:33 +0300

    Task2

--- Seminar2/HomeWork/Task2HW.py
+++ Seminar2/HomeWork/Task2HW.py
@@ -2,3 +2,11 @@
 #  выдает набор произведений чисел от 1 до N.
 #     *Пример:*
 #     - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+# import math
+n = int(input('Print your N: '))
+# print(math.factorial(n))
+fact = 1
+for i in range(1,n+1):
+    fact*=i
+    print(fact, end =' ')
