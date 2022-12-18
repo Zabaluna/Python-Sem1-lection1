3d60e05c0df621e29788dc801e9a60c8b3d95f08
Наталья Ярцева <zabaluna@mail.ru>
2022-12-18 23:43:07 +0300

    tasks

--- Seminar2/HomeWork/Task2HW.py
+++ Seminar2/HomeWork/Task2HW.py
@@ -3,10 +3,21 @@
 #     *Пример:*
 #     - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-# import math
+
 n = int(input('Print your N: '))
-# print(math.factorial(n))
+
 fact = 1
 for i in range(1,n+1):
     fact*=i
     print(fact, end =' ')
+
+# Функция факториала из листа подсказки))    
+n = int(input('Print your N: '))    
+def fact(n):  # Обычная функция факториала
+    pr=1
+    a=[]
+    for i in range(1,n+1):
+        pr=pr*i
+        a.append(pr)
+    return a
+print(fact(n))
--- Seminar2/HomeWork/Task3HW.py
+++ Seminar2/HomeWork/Task3HW.py
@@ -3,3 +3,10 @@
 # и выведите на экран их сумму. 
 #     *Пример:*
 #     - Для n=4 {1: 2, 2: 2.25, 3: 2.37, 4: 2.44}
+n = int(input('Priny your N: '))
+my_dict = {}
+for i in range(1,n+1):
+    my_dict[i] = (1+1/i)**i
+    my_dict[i] = round(my_dict[i],2)
+print(my_dict)
+
