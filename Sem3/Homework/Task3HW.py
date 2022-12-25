d958e0b021c6e691dae32a8f9f613f0e95db0035
Наталья Ярцева <zabaluna@mail.ru>
2022-12-25 16:28:35 +0300

    task2 ans task3

--- Sem3/Homework/Task3HW.py
+++ Sem3/Homework/Task3HW.py
@@ -2,3 +2,18 @@
 #  которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 # *Пример:*
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+import random
+
+num = int(input("Ведите число: ")) # = 5
+my_list = []
+for i in range(num):
+    my_list.append(random.uniform(0.00, 100.99))
+    my_list[i]=round(my_list[i],2)
+print(f'{my_list} -->')
+
+my_new_list = [round(i%1,2) for i in my_list if i%1 != 0]
+print(f'{my_new_list} -->')
+print(f'{max(my_new_list)}-{min(my_new_list)} -->')
+result = max(my_new_list)-min(my_new_list)
+print(round(result,2))
