2dd1f9d3dc4602a6735cad6182110a057eaae6cd
Наталья Ярцева <zabaluna@mail.ru>
2022-12-27 22:58:34 +0300

    Task3

--- Seminar4/HOMEWORK/Task1HW.py
+++ Seminar4/HOMEWORK/Task1HW.py
@@ -4,9 +4,10 @@
 
 
 from decimal import *
+from math import pi
 d = int(input('Print the given number precision '))
 getcontext().prec = d
-result = Decimal(3.141592653589793238462643383279)/Decimal(1)
+result = Decimal(pi)/Decimal(1)
 print(result)
 
 # from math import pi
--- Seminar4/HOMEWORK/Task3HW.py
+++ Seminar4/HOMEWORK/Task3HW.py
@@ -1,3 +1,31 @@
 # 3 Задайте последовательность чисел.
-#  Напишите программу, которая выведет список 
+#  Напишите программу, которая выведет список
 #  неповторяющихся элементов исходной последовательности.
+
+
+# my_list = [2, 4, 7, 8, 34, 90, -34, 2, 7, -90, 7] #2 и 7
+# print(my_list)
+# my_new_list = []
+# for i in range(0, len(my_list)):
+#     for j in range(i+1, len(my_list)):
+#         if my_list[i] == my_list[j]:
+#            my_new_list.append(my_list[j])
+# print(my_new_list) наоборот вывела только повторяющиеся значения
+
+# my_list = [2, 4, 7, 8, 34, 90, -34, 2, 7, -90, 7]
+# print(my_list)
+# my_set = set(my_list)
+# print(my_set) убирает только задвоенные значения, тк множество хранит уникальные значения
+
+
+my_list = [2, 4, 7, 8, 34, 90, -34, 2, 7, -90, 7] #2 и 7
+print(my_list)
+my_new_list = [] 
+for i in my_list:
+    count=0
+    for j in my_list:
+        if i==j:
+            count+=1
+    if count == 1:
+        my_new_list.append(i)
+print(my_new_list)      
