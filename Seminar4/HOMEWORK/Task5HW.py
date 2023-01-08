1f490d689661199c4d6cfd6e24006ab1dae22948
Наталья Ярцева <zabaluna@mail.ru>
2023-01-09 00:20:42 +0300

    addition polinominals

--- Seminar4/HOMEWORK/Task5HW.py
+++ Seminar4/HOMEWORK/Task5HW.py
@@ -1,2 +1,50 @@
 # 5 Даны два файла, в каждом из которых находится запись многочлена.
 #  Задача - сформировать файл, содержащий сумму многочленов.
+
+
+# Открываем 2 наших записанных файла txt, выводим на печать
+with open('new_equation_k.txt', 'r') as filek:
+    first1 = filek.read()
+with open('new_equation2_k.txt', 'r') as filek:
+    second2 = filek.read()
+
+print(f"{first1} Первый многочлен ")
+print(f"{second2} Второй многочлен")
+
+# переводим наши многочлены обратно в словарь: ключ значение
+def backdictinary(polinomilal: str) -> dict:
+    polinomilal = polinomilal.replace('+', '').replace(' x',' 1*x').replace('*x ', '*x**1')
+    polinomilal = polinomilal.split()[:-2] # берем срез, чтобы 0 и = отвалились
+    dict_equation = {}
+    # for i in range(len(polinomilal)):
+    #    polinomilal[i] = polinomilal[i].split('*x**')
+    for item in polinomilal:
+        i = item.split('*x**')
+        if len(i) > 1:
+            dict_equation[int(i[1])] = int(i[0])
+        else:
+            dict_equation[0] = int(i[0])
+    return dict_equation
+
+
+#  выводим на печать первый и второй словарь, который получился из многочленов 
+dict_equation = backdictinary(first1)
+print(dict_equation)
+
+dict_equation = backdictinary(second2)
+print(dict_equation)
+
+
+# суммируем наши многочлены по ключам(если ключа нет,то берется ноль)
+def addition(first_eq: dict, second_eq: dict):
+    final_eq = {}
+    final_eq.update(first_eq)
+    final_eq.update(second_eq)
+    for key in final_eq:
+        final_eq[key] = first_eq.get(key, 0) + second_eq.get(key, 0)
+    return final_eq    
+
+
+final_eq = addition(backdictinary(first1), backdictinary(second2))
+print(final_eq)
+
